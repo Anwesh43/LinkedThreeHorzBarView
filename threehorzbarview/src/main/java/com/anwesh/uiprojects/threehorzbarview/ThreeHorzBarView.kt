47f71d2c11a83bfa51eb7e5cf49a6cc037ef0b9c
Anwesh43 <anweshthecool0@gmail.com>
2018-10-07 02:27:06 +0530

    addinf ThreeHorzBarView class

--- threehorzbarview/src/main/java/com/anwesh/uiprojects/threehorzbarview/ThreeHorzBarView.kt
+++ threehorzbarview/src/main/java/com/anwesh/uiprojects/threehorzbarview/ThreeHorzBarView.kt
@@ -13,3 +13,21 @@ import android.graphics.RectF
 import android.content.Context
 
 val nodes : Int = 5
+
+class ThreeHorzBarView(ctx : Context) : View(ctx) {
+
+    private val paint : Paint = Paint(Paint.ANTI_ALIAS_FLAG)
+
+    override fun onDraw(canvas : Canvas) {
+
+    }
+
+    override fun onTouchEvent(event : MotionEvent) : Boolean {
+        when (event.action) {
+            MotionEvent.ACTION_DOWN -> {
+
+            }
+        }
+        return true
+    }
+}
