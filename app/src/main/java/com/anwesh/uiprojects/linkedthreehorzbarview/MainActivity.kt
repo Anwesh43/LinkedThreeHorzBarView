1bbf09915865262c9e36100a84dd5958b4580a7b
Anwesh43 <anweshthecool0@gmail.com>
2018-10-07 03:03:34 +0530

    adding ThreeHorzBarView in activity

--- app/src/main/java/com/anwesh/uiprojects/linkedthreehorzbarview/MainActivity.kt
+++ app/src/main/java/com/anwesh/uiprojects/linkedthreehorzbarview/MainActivity.kt
@@ -2,10 +2,12 @@ package com.anwesh.uiprojects.linkedthreehorzbarview
 
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import com.anwesh.uiprojects.threehorzbarview.ThreeHorzBarView
 
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+        ThreeHorzBarView.create(this)
     }
 }
