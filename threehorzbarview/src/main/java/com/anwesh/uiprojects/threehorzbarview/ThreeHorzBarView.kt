cadb7857c84499e331a7db9ab5cd6b5ba475469d
Anwesh43 <anweshthecool0@gmail.com>
2018-10-07 02:25:10 +0530

    importing libraries and defining nodes

--- threehorzbarview/src/main/java/com/anwesh/uiprojects/threehorzbarview/ThreeHorzBarView.kt
+++ threehorzbarview/src/main/java/com/anwesh/uiprojects/threehorzbarview/ThreeHorzBarView.kt
@@ -3,3 +3,13 @@ package com.anwesh.uiprojects.threehorzbarview
 /**
  * Created by anweshmishra on 07/10/18.
  */
+
+import android.view.View
+import android.view.MotionEvent
+import android.graphics.Canvas
+import android.graphics.Paint
+import android.graphics.Color
+import android.graphics.RectF
+import android.content.Context
+
+val nodes : Int = 5
